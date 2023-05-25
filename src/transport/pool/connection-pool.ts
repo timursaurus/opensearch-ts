1dd477e745ba3149ec759a5263677bb99212ec2a
Timur Bolotov <bolotovtmr@gmail.com>
2023-05-25 23:00:06 +0600

    feat: connection pool methods
    Signed-off-by: Timur Bolotov <bolotovtmr@gmail.com>

--- src/transport/pool/connection-pool.ts
+++ src/transport/pool/connection-pool.ts
@@ -64,31 +64,80 @@ export class ConnectionPool extends BaseConnectionPool {
     this.resurrectTimeout = 1000 * 60;
     this.resurrectTimeoutCutoff = 5;
     this.pingTimeout = options.pingTimeout ?? 3000;
-    const _resurrectStrategy = options.resurrectStrategy ?? "ping";
 
-    this.resurrectStrategy = ConnectionPool.resurrectStrategies[_resurrectStrategy];
+    const _strategy = options.resurrectStrategy ?? "ping";
+    this.resurrectStrategy = ConnectionPool.resurrectStrategies[_strategy];
 
-    assert(
-      this.resurrectStrategy != null,
-      `Invalid resurrection strategy: '${_resurrectStrategy}'`
-    );
+    assert(this.resurrectStrategy != null, `Invalid resurrection strategy: '${_strategy}'`);
   }
 
+  /**
+   * Marks a connection as 'dead'.
+   * If needed adds the connection to the dead list
+   * and then increments the `deadCount`.
+   *
+   * @param connection
+   */
   markAlive(connection: Connection): this {
-    const { id: _id } = connection;
-    debug(`Marking as 'alive' connection '${_id}'`);
-    const index = this.dead.indexOf(_id);
+    const { id } = connection;
+    debug(`Marking as 'alive' connection '${id}'`);
+    const index = this.dead.indexOf(id);
     if (index > -1) {
       this.dead.splice(index, 1);
     }
+
     connection.status = Connection.statuses.ALIVE;
     connection.deadCount = 0;
     connection.resurrectTimeout = 0;
     return this;
   }
 
+  /**
+   * Marks a connection as 'dead'.
+   * If needed, adds the connection to the dead list
+   * and then increments the `deadCount`.
+   *
+   * @param connection
+   */
   markDead(connection: Connection): this {
-    const { id: _id } = connection;
+    const { id, deadCount } = connection;
+    debug(`Marking as 'dead' connection '${id}'`);
+    /**
+     * It might happen that `markDead` is called just after
+     * a pool update, and in such case we will add to the dead
+     * list a node that no longer exist. The following check verify
+     * that the connection is still part of the pool before
+     * marking it as dead.
+     */
+    if (!this.dead.includes(id) && this.connections.some((c) => c.id === id)) {
+      this.dead.push(id);
+    }
+    connection.status = Connection.statuses.DEAD;
+    connection.deadCount += 1;
+
+    /**
+     * ResurrectTimeout formula:
+     * `resurrectTimeout * 2 ** min(deadCount - 1, resurrectTimeoutCutoff)`
+     */
+    const _resurrectTimeout =
+      Date.now() +
+      this.resurrectTimeout * Math.pow(2, Math.min(deadCount - 1, this.resurrectTimeoutCutoff));
+
+    connection.resurrectTimeout = _resurrectTimeout;
+
+    /**
+     * Sort the dead connections list in ascending order based on the
+     * `resurrectTimeout` property.
+     */
+    this.dead.sort((a, b) => {
+      const conn1 = this.connections.find((c) => c.id === a);
+      const conn2 = this.connections.find((c) => c.id === b);
+      if (conn1 && conn2) {
+        return conn1.resurrectTimeout - conn2.resurrectTimeout;
+      }
+      return 0;
+    });
+
     return this;
   }
 
@@ -109,6 +158,15 @@ export class ConnectionPool extends BaseConnectionPool {
     });
   }
 
+  /**
+   * Returns an alive connection if present,
+   * otherwise returns a dead connection.
+   * By default, it filters the `cluster_manager` or `master` only nodes.
+   * It uses the selector to choose which
+   * connection to return
+   * @param options
+   * @returns { Connection | null }
+   */
   getConnection(options: GetConnectionOptions): Connection | null {
     const filter = options.filter ?? (() => true);
     const selector = options.selector ?? ((c) => c[0]);
@@ -130,9 +188,13 @@ export class ConnectionPool extends BaseConnectionPool {
     return selector(connections);
   }
 
-  // callback function overload when no args provided its optional
-  // but if args are provided then callback is mandatory
-
+  /**
+   * If enabled, tries to resurrect a connection with the given
+   * resurrect strategy (`'ping'`, `'optimistic'`, `'none'`).
+   * @param options
+   * @param callback
+   * @returns {void}
+   */
   resurrect(options: ResurrectOptions, callback: ResurrectCallback = NOOP): void {
     if (this.resurrectStrategy === 0 || this.dead.length === 0) {
       debug("Nothing to resurrect");
@@ -144,46 +206,62 @@ export class ConnectionPool extends BaseConnectionPool {
      * the dead list is sorted in ascending order based on the timeout
      * so the first element will always be the one with the smaller timeout
      */
-    const _connection = this.connections.find((c) => c.id === this.dead[0]) as Connection;
-    if ((options?.now || Date.now()) < _connection?.resurrectTimeout) {
+    const connection = this.connections.find((c) => c.id === this.dead[0]) as Connection;
+    if ((options?.now || Date.now()) < connection?.resurrectTimeout) {
       debug("Nothing to resurrect, the timeout is not expired yet");
       callback();
       return;
     }
-    const { id: _id } = _connection;
+    const { id } = connection;
 
     /**
      * Ping strategy
      */
     if (this.resurrectStrategy === ConnectionPool.resurrectStrategies.ping) {
-      _connection.request(
+      connection.request(
         { method: "HEAD", path: "/", timeout: this.pingTimeout },
-        (_error, _response) => {
+        (error, response) => {
           let _isAlive = true;
-          const _statusCode = _response?.statusCode ?? 0;
+          const statusCode = response?.statusCode ?? 0;
           if (
-            _error != null ||
-            _statusCode === HTTP_STATUS_CODE.BAD_GATEWAY ||
-            _statusCode === HTTP_STATUS_CODE.SERVICE_UNAVAILABLE ||
-            _statusCode === HTTP_STATUS_CODE.GATEWAY_TIMEOUT
+            error != null ||
+            statusCode === HTTP_STATUS_CODE.BAD_GATEWAY ||
+            statusCode === HTTP_STATUS_CODE.SERVICE_UNAVAILABLE ||
+            statusCode === HTTP_STATUS_CODE.GATEWAY_TIMEOUT
           ) {
-            debug(`Resurrect: connection '${_id}' is still dead`);
-            this.markDead(_connection);
+            debug(`Resurrect: connection '${id}' is still dead`);
+            this.markDead(connection);
             _isAlive = false;
           } else {
-            debug(`Resurrect: connection '${_id}' is alive`);
-            this.markAlive(_connection);
+            debug(`Resurrect: connection '${id}' is alive`);
+            this.markAlive(connection);
           }
           this.emit("resurrect", null, {
             strategy: "ping",
             name: options.name,
             request: { id: options.requestId },
             isAlive: _isAlive,
-            connection: _connection,
+            connection,
           });
-          callback(_isAlive, _connection);
+          callback(_isAlive, connection);
         }
       );
+    } else {
+      /**
+       * Optimistic strategy
+       */
+      debug(`Resurrect: optimistic resurrection of connection '${id}'`);
+      this.dead.splice(this.dead.indexOf(id), 1);
+      this.markAlive(connection);
+      const isAlive = true;
+      this.emit("resurrect", null, {
+        strategy: "optimistic",
+        name: options.name,
+        request: { id: options.requestId },
+        isAlive,
+        connection,
+      });
+      callback(isAlive, connection);
     }
   }
 }
